0132daef7690161dd1468a9b0365be3559f5fff9
mogalemd <mogalemd@hotmail.com>
2016-08-10 16:43:50 +0200

    new changes

--- JobTest/Controllers/JobsController.cs
+++ JobTest/Controllers/JobsController.cs
@@ -10,28 +10,9 @@ namespace JobTest.Controllers
 {
     public class JobsController : Controller
     {
-        //
-        // GET: /Jobs/
-         [HttpGet]
-        public ActionResult Index()
-        {
 
-            string file = Server.MapPath("~/App_Data/jobs.json");
-
-            //deserialie JSON from file
-            string Json = System.IO.File.ReadAllText(file);
-             JavaScriptSerializer jss = new JavaScriptSerializer();
-             //var data = jss.Deserialize<RootObject>(Json);
-             var data = Newtonsoft.Json.JsonConvert.DeserializeObject<RootObject>(Json);
-             var list = (from c in data.jobs
-                         select c);
-             return View(list);
-        }
-
-         [HttpPost]
-         public ActionResult Index(string SearchKey)
+         public ActionResult Index(string q, string S)
          {
-
              string file = Server.MapPath("~/App_Data/jobs.json");
 
              //deserialie JSON from file
@@ -39,45 +20,50 @@ namespace JobTest.Controllers
              JavaScriptSerializer jss = new JavaScriptSerializer();
              //var data = jss.Deserialize<RootObject>(Json);
              var data = Newtonsoft.Json.JsonConvert.DeserializeObject<RootObject>(Json);
-             var list = (from c in data.jobs
-                         where (c.client + ' ' + c.jobnumber.ToString() + ' ' + c.jobname + ' ' + c.status).Contains(SearchKey)
-                        // where c.client.Contains(SearchKey) 
-                         select c);
+             var jobs = (from j in data.jobs
+                         select j);
 
-           
-             return View(list);
+             int id = Convert.ToInt32(Request["SearchType"]);
+             var searchParameter = "Searching";
+      
+
+             if (!string.IsNullOrWhiteSpace(q))
+             {
+                 switch (id)
+                 {
+                     case 0:
+                         int iQ = Convert.ToInt32(q);
+                         jobs = jobs.Where(j => j.jobnumber.Equals(iQ));
+                         searchParameter += " Job Number for ' " + q + " '";
+                         break;
+                     case 1:
+                         jobs = jobs.Where(j => j.client.Contains(q));
+                         searchParameter += " Clent for ' " + q + " '";
+                         break;
+                     case 2:
+                         jobs = jobs.Where(j => j.jobname.Contains(q));
+                         searchParameter += " Job Name for '" + q + "'";
+                         break;
+                     case 3:
+                         DateTime duedate = Convert.ToDateTime(q);
+                         jobs = jobs.Where(j => j.due.Equals(duedate));
+                         searchParameter += " Due Date for '" + q + "'";
+                         break;
+                     case 4:
+                         jobs = jobs.Where(j => j.status.Contains(q));
+                         searchParameter += " Status for '" + q + "'";
+                         break;
+                 }
+             }
+             else
+             {
+                 searchParameter += "ALL";
+             }
+             ViewBag.SearchParameter = searchParameter;
+             return View(jobs);
          }
 
-        // public List<Job> JobsData(string SearchKey)
-        //{
-        //    List<Job> jobs = new List<Job>();
-        //    string file = Server.MapPath("~/App_Data/jobs.json");
 
-        //    deserialie JSON from file
-        //    string Json = System.IO.File.ReadAllText(file);
-        //    JavaScriptSerializer jss = new JavaScriptSerializer();
-        //    var data = jss.Deserialize<RootObject>(Json);
-        //    var data = Newtonsoft.Json.JsonConvert.DeserializeObject<RootObject>(Json);
-        //    var jobsitem = from c in data.jobs 
-        //                where SearchKey == "" ? true : 
-        //                c.client.Contains(SearchKey)
-        //                select c;
-        //    foreach (var item in jobsitem)
-        //    {
-        //        jobs.Add(new Job{
-        //            client = item.client,
-        //            jobnumber = item.jobnumber,
-        //            jobname = item.jobname,
-        //            due = item.due,
-        //            status = item.status
-                   
-        //        });
-
-              
-        //    }
-        //    return jobs;
-          
-        //}
 
     }
 }
